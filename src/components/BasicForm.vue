27549a56afd1fd027692a38ad2473b41bcab1fbe
nglushien17 <nglushien@ushmm.org>
2018-11-29 13:35:44 -0500

    updated the basicform and lightbox components

--- src/components/BasicForm.vue
+++ src/components/BasicForm.vue
@@ -1,14 +1,42 @@
 <template>
   <div>
     <b-form>
-      <label class="sr-only" for="inlineFormInputName2">Lightbox Title</label>
-      <b-input class="mb-2 mr-sm-2 mb-sm-0" id="inlineFormInputName2" placeholder="Title" v-model="lbTitle" />
-      <label class="sr-only" for="inlineFormInputName3">Lightbox CTA</label>
-      <b-input class="mb-2 mr-sm-2 mb-sm-0" id="inlineFormInputName3" placeholder="CTA" v-model="lbCTA" />
+      <b-form-group id="exampleInputGroup1"
+                    label="Do you need a title?"
+                    label-for="toggleTitle">
+        <toggle-button id="toggleTitle" @change="disabled = (disabled + 1) % 2" :value="false" :labels="{checked: 'On', unchecked: 'Off'}" />
+        <label class="sr-only" for="inlineFormInputName2">Lightbox Title</label>
+        <b-input-group class="mb-md-4">
+          <b-form-input :maxlength="maxTitle" id="inlineFormInputName2" placeholder="Title" v-model="lbTitle" :disabled="disabled == 0 ? true : false"></b-form-input>
+          <b-input-group-text slot="append">
+            <strong v-text="(maxTitle - lbTitle.length)"></strong>
+          </b-input-group-text>
+        </b-input-group>
+      </b-form-group>
+
+      <!-- <label class="sr-only" for="inlineFormInputName3">Lightbox CTA</label>
+      <b-input-group class="mb-md-4">
+        <b-form-input :maxlength="maxCTA" id="inlineFormInputName3" placeholder="CTA" v-model="lbCTA"></b-form-input>
+        <b-input-group-text slot="append">
+          <strong v-text="(maxCTA - lbCTA.length)"></strong>
+        </b-input-group-text>
+      </b-input-group> -->
+      
       <label class="sr-only" for="inlineFormInputName4">Lightbox Text</label>
-      <b-input class="mb-2 mr-sm-2 mb-sm-0" id="inlineFormInputName4" placeholder="Text" v-model="lbText" />
+      <b-input-group class="mb-md-4">
+        <b-form-input :maxlength="maxText" id="inlineFormInputName4" placeholder="Text" v-model="lbText"></b-form-input>
+        <b-input-group-text slot="append">
+          <strong v-text="(maxText - lbText.length)"></strong>
+        </b-input-group-text>
+      </b-input-group>
+      
       <label class="sr-only" for="inlineFormInputName5">Lightbox Button</label>
-      <b-input class="mb-2 mr-sm-2 mb-sm-0" id="inlineFormInputName5" placeholder="Button" v-model="lbButton" />
+      <b-input-group class="mb-md-4">
+        <b-form-input :maxlength="maxButton" id="inlineFormInputName5" placeholder="Button" v-model="lbButton"></b-form-input>
+        <b-input-group-text slot="append">
+          <strong v-text="(maxButton - lbButton.length)"></strong>
+        </b-input-group-text>
+      </b-input-group>
     </b-form>
 
     <LightBox :title="lbTitle" :cta="lbCTA" :text="lbText" :button="lbButton" />
@@ -16,9 +44,9 @@
 </template>
 
 <script>
+
 import { serverBus } from '../main';
 import LightBox from './LightBox.vue';
-
 
 export default {
   name: 'BasicForm',
@@ -27,20 +55,23 @@ export default {
   },
   data() {
     return { 
-      lbTitle: this.title,
-      lbCTA: this.cta,
-      lbText: this.text,
-      lbButton: this.button
+      maxTitle: 15,
+      maxText: 150,
+      maxButton: 10,
+      lbTitle: '',
+      lbCTA: '',
+      lbText: '',
+      lbButton: '',
+      disabled: 0
     }
   },
   watch: {
     sendData: function () {
       serverBus.$emit('test', this.title);
-      serverBus.$emit('test', this.cta);
+      // serverBus.$emit('test', this.cta);
       serverBus.$emit('test', this.text);
       serverBus.$emit('test', this.button);
     }
-    
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App.vue'
+import ToggleButton from 'vue-js-toggle-button'
 
+Vue.use(ToggleButton)
 Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
